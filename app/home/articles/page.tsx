c95efb27812f7d6dc6159fbecb24b3331a320b40
kuruxxii <zzzihanlin@gmail.com>
2024-05-29 12:58:46 +0800

    add topic filtering and pagination

--- app/home/articles/page.tsx
+++ app/home/articles/page.tsx
@@ -3,14 +3,9 @@
 import PageTitle from "@/app/ui/PageTitle";
 import NewsTicker from "@/app/ui/NewsTicker";
 import { useState, useEffect } from "react";
-
-type Topic =
-  | "个人成长"
-  | "商业财经"
-  | "科技前沿"
-  | "人文社科"
-  | "职场专题"
-  | "校园学习专题";
+import { Button } from "@/components/ui/button";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import Pagination from "@/app/ui/Pagination";
 
 type ArticlePreview = {
   slug: string;
@@ -19,12 +14,53 @@ type ArticlePreview = {
   introduction: string;
   author: string;
   read: number;
-  topic: Topic;
+  topic: string;
 };
+type Topic =
+  | "all"
+  | "personal"
+  | "business"
+  | "technology"
+  | "humanities"
+  | "workplace"
+  | "school";
 
 export default function ArticlePreviewPage() {
   const [articlePreviews, setArticlePreviews] = useState<ArticlePreview[]>([]);
-  const [selectedTopic, setSelectedTopic] = useState<Topic | "全部">("全部");
+  const topicMap = {
+    all: "全部",
+    personal: "个人成长",
+    business: "商业财经",
+    technology: "科技前沿",
+    humanities: "人文社科",
+    workplace: "职场专题",
+    school: "校园学习专题",
+  };
+  const topics: Topic[] = [
+    "all",
+    "personal",
+    "business",
+    "technology",
+    "humanities",
+    "workplace",
+    "school",
+  ];
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const selectedTopic = searchParams.get("topic") || "all";
+  const currentPage = Number(searchParams.get("page")) || 1;
+  const [totalPages, setTotalPages] = useState<number>(100);
+  const handleFilter = (topic: string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", "1");
+    if (topic) {
+      params.set("topic", topic);
+    } else {
+      params.delete("topic");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  };
   return (
     <main className="flex flex-col min-h-screen max-w-[95rem] w-full mx-auto lg:pt-0 sm:pt-4 xs:pt-2 lg:pb-4 md:pb-4 sm:pb-2 xs:pb-2">
       <PageTitle
@@ -32,6 +68,26 @@ export default function ArticlePreviewPage() {
         imgAlt="The word 'Magazine' in bold, uppercase lettering"
       />
       <NewsTicker />
+      <div className="flex flex-wrap justify-between items-center gap-2 md:gap-0 my-6">
+        <p className="font-semibold uppercase">领域专题</p>
+        {articlePreviews && (
+          <div className="flex flex-wrap gap-2">
+            {topics.map((topic) => (
+              <Button
+                className={`px-3 py-2 bg-white text-black hover:bg-black hover:text-white border border-black rounded-full ${
+                  topic === selectedTopic
+                    ? "bg-black text-white"
+                    : "border-black"
+                }`}
+                key={topic}
+                onClick={() => handleFilter(topic)}>
+                {topicMap[topic]}
+              </Button>
+            ))}
+          </div>
+        )}
+      </div>
+      <Pagination totalPages={totalPages} />
     </main>
   );
 }
