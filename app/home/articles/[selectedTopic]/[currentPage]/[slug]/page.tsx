47c25164eb1314fc5b68ca108c64e2aebe361ca6
kuruxxii <zzzihanlin@gmail.com>
2024-06-02 14:57:30 +0800

    add not data page

--- app/home/articles/[selectedTopic]/[currentPage]/[slug]/page.tsx
+++ app/home/articles/[selectedTopic]/[currentPage]/[slug]/page.tsx
@@ -60,49 +60,50 @@ export default function ArticlePage() {
   }, [params.slug]);
   if (!article) {
     return (
+      <main className="max-w-[95rem] w-full h-screen mx-auto sm:pt-4 xs:pt-2 lg:pb-4 md:pb-4 sm:pb-2 xs:pb-2 flex justify-center items-center">
+        <p className="text-2xl pb-16">前面的區域，以後再來探索吧！</p>
+      </main>
+    );
+  } else {
+    return (
       <main className="max-w-[95rem] w-full mx-auto sm:pt-4 xs:pt-2 lg:pb-4 md:pb-4 sm:pb-2 xs:pb-2">
-        <p>Article not found</p>;
+        <PostNavigation
+          href={`/home/articles/?page=${params.currentPage}&topic=${params.selectedTopic}`}>
+          精选文章
+        </PostNavigation>
+        <article className="grid md:grid-cols-2 gap-6 md:gap-6 pb-6 md:pb-24">
+          <h2 className="text-6xl font-black">{article.title}</h2>
+          <div>
+            <span className="font-bold text-2xl">Harry说：</span>
+            <p className="text-lg">{article.introduction}</p>
+          </div>
+        </article>
+        <div className="flex flex-col md:flex-row justify-between gap-6 md:gap-0 mb-8">
+          <div className="flex flex-col sm:flex-row md:items-center gap-2 sm:gap-6">
+            <span className="flex flex-wrap">
+              <p className="font-semibold pr-2">作者</p>
+              <p>{article.author}</p>
+            </span>
+            <span className="flex flex-wrap">
+              <p className="font-semibold pr-2">日期</p>
+              {format(new Date(article.createdAt), "yyyy-MM-dd")}
+            </span>
+            <span className="flex flex-wrap">
+              <p className="font-semibold pr-2">预计阅读时间</p>
+              <p>{`${article.read}分钟`}</p>
+            </span>
+          </div>
+          <span className="px-3 py-2 border border-black rounded-full w-fit">
+            <p className="uppercase">{article.topic}</p>
+          </span>
+        </div>
+        <div>
+          <img src={article.coverUrl} alt={article.title} className="" />
+        </div>
+        <article className="max-w-[900px] prose-lg lg:prose-2xl mx-auto mt-6 mb-24 text-black">
+          <Markdown>{article.content}</Markdown>
+        </article>
       </main>
     );
   }
-  return (
-    <main className="max-w-[95rem] w-full mx-auto sm:pt-4 xs:pt-2 lg:pb-4 md:pb-4 sm:pb-2 xs:pb-2">
-      <PostNavigation
-        href={`/home/articles/?page=${params.currentPage}&topic=${params.selectedTopic}`}>
-        精选文章
-      </PostNavigation>
-      <article className="grid md:grid-cols-2 gap-6 md:gap-6 pb-6 md:pb-24">
-        <h2 className="text-6xl font-black">{article.title}</h2>
-        <div>
-          <span className="font-bold text-2xl">Harry说：</span>
-          <p className="text-lg">{article.introduction}</p>
-        </div>
-      </article>
-      <div className="flex flex-col md:flex-row justify-between gap-6 md:gap-0 mb-8">
-        <div className="flex flex-col sm:flex-row md:items-center gap-2 sm:gap-6">
-          <span className="flex flex-wrap">
-            <p className="font-semibold pr-2">作者</p>
-            <p>{article.author}</p>
-          </span>
-          <span className="flex flex-wrap">
-            <p className="font-semibold pr-2">日期</p>
-            {format(new Date(article.createdAt), "yyyy-MM-dd")}
-          </span>
-          <span className="flex flex-wrap">
-            <p className="font-semibold pr-2">预计阅读时间</p>
-            <p>{`${article.read}分钟`}</p>
-          </span>
-        </div>
-        <span className="px-3 py-2 border border-black rounded-full w-fit">
-          <p className="uppercase">{article.topic}</p>
-        </span>
-      </div>
-      <div>
-        <img src={article.coverUrl} alt={article.title} className="" />
-      </div>
-      <article className="max-w-[900px] prose-lg lg:prose-2xl mx-auto mt-6 mb-24 text-black">
-        <Markdown>{article.content}</Markdown>
-      </article>
-    </main>
-  );
 }
